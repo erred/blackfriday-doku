1578e1cf90b6b76d79a29bf130b5b98710de2866
Sean Liao <seankhliao@gmail.com>
2020-02-16 13:48:38 +0100

    remove zerolog

--- cmd/md2doku/main.go
+++ cmd/md2doku/main.go
@@ -5,11 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 	"github.com/russross/blackfriday"
 	doku "github.com/seankhliao/blackfriday-doku"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 const (
@@ -17,30 +14,11 @@ const (
 )
 
 func main() {
-	initLog()
-
 	b, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
-		log.Fatal().Err(err).Msg("read file")
+		fmt.Printf("read file: %v", err)
+		os.Exit(1)
 	}
 
-	b = blackfriday.Run(b, blackfriday.WithRenderer(doku.NewRenderer()), blackfriday.WithExtensions(es))
-
-	fmt.Printf("\n%s\n", b)
-
-}
-
-func initLog() {
-	logfmt := os.Getenv("LOGFMT")
-	if logfmt != "json" {
-		logfmt = "text"
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, NoColor: !terminal.IsTerminal(int(os.Stdout.Fd()))})
-	}
-
-	level, _ := zerolog.ParseLevel(os.Getenv("LOGLVL"))
-	if level == zerolog.NoLevel {
-		level = zerolog.InfoLevel
-	}
-	log.Info().Str("FMT", logfmt).Str("LVL", level.String()).Msg("log initialized")
-	zerolog.SetGlobalLevel(level)
+	fmt.Printf("%s\n", blackfriday.Run(b, blackfriday.WithRenderer(doku.NewRenderer()), blackfriday.WithExtensions(es)))
 }
